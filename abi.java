7d4f78eee134230a6e0f47536e9c67bd6b3e1002
Abilesh R <abilesh.r@tringapps.com>
2024-11-28 15:44:01 +0530

    Abi

--- abi.java
+++ abi.java
@@ -2,7 +2,8 @@
 public class abi {
 	public static void main(String args[])
 	{
-	System.out.println("Hello World"); //Prints on screen “Hello World”
+	System.out.println("Hello World"); 
+	System.out.println("Hello World");//Prints on screen “Hello World”
 	}
 
 }
